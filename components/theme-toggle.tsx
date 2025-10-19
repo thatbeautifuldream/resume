728735147b50d87b103d91906cf70ba2c85a6e2a
Milind Mishra <milind.mishra4@gmail.com>
2025-10-19 20:06:06 +0530

    fix: theme toggle icon fill and size fix

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -14,8 +14,12 @@ export function ThemeToggle() {
 
   return (
     <Button variant="ghost" size="icon" onClick={toggleTheme}>
-      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <Sun
+        className="size-3 scale-100 rotate-0 transition-all fill-current dark:scale-0 dark:-rotate-90"
+      />
+      <Moon
+        className="absolute size-3 scale-0 rotate-90 transition-all fill-current dark:scale-100 dark:rotate-0"
+      />
       <span className="sr-only">Toggle theme</span>
     </Button>
   );
