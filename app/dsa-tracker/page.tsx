b42afc317bc9e77028004e501c29dd1a9b6d10b8
Milind Mishra <milind.mishra4@gmail.com>
2025-11-09 02:55:46 +0530

    feat: add dsa tracker route

---
+++ app/dsa-tracker/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <iframe
+      src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTImaPMBQOPWtX8cPbszbcW0wjATDpefzliCfQCLqbppGcC0cwinIxG9WlQAVNYKZponeNffLh4lD5S/pubhtml?widget=false&amp;headers=false"
+      title="DSA Tracker Spreadsheet"
+      className="h-screen w-full border-0"
+    ></iframe>
+  );
+}
