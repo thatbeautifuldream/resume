f135d0e1361639b94c213fb8bc2dd8388ca2d3da
Milind Mishra <milind.mishra4@gmail.com>
2026-01-14 04:13:32 +0530

    fix: cleanup

--- app/pinned/page.tsx
+++
@@ -1,9 +0,0 @@
-import { pinnedProjects } from "@/lib/pinned-projects";
-
-export default function Page() {
-  return (
-    <code className="whitespace-pre-wrap break-words">
-      {JSON.stringify(pinnedProjects, null, 2)}
-    </code>
-  );
-}
